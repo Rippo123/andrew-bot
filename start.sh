70701cfa51637a6e4816957bfeb5d9f0235a428b
Rippo123 <98217663+Rippo123@users.noreply.github.com>
2022-05-20 21:29:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/Rippo123/andrew-bot/andrew-bot.git /andrew-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /My-Dream
+  git clone $UPSTREAM_REPO /andrew-bot
 fi
-cd /My-Dream
+cd /andrew-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
